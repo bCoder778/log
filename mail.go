b4c0e8a9fd802e0b062fc1fb8833d4284a3dc5da
bCoder778 <bCoder778@gmail.com>
2021-08-11 10:09:35 +0800

    Update mail.go

--- mail.go
+++ mail.go
@@ -11,6 +11,7 @@ type EMail struct {
 
 type EMailOption struct {
 	User   string
+	Name   string
 	Pass   string
 	Host   string
 	Port   string
@@ -28,7 +29,7 @@ func (e *EMail) SendEmail(subject string, body string) error {
 	if err != nil {
 		return err
 	}
-	m.SetHeader("From", m.FormatAddress(e.opt.User, "")) //这种方式可以添加别名，即“XX官方”
+	m.SetHeader("From", m.FormatAddress(e.opt.User, e.opt.Name)) //这种方式可以添加别名，即“XX官方”
 	//说明：如果是用网易邮箱账号发送，以下方法别名可以是中文，如果是qq企业邮箱，以下方法用中文别名，会报错，需要用上面此方法转码
 	//m.SetHeader("From", "FB Sample"+"<"+mailConn["user"]+">") //这种方式可以添加别名，即“FB Sample”， 也可以直接用<code>m.SetHeader("From",mailConn["user"])</code> 读者可以自行实验下效果
 	//m.SetHeader("From", mailConn["user"])
