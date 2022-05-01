563b711a60ee7b11ffcaf6950d78362b7229b542
bCoder778 <87968445+QDanlSc@users.noreply.github.com>
2022-05-01 08:14:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -209,10 +209,11 @@ func Warn(msg ...string) {
 	if _conf.Level > WARN {
 		return
 	}
-	if _conf != nil {
+	if _conf != nil && _conf2 != nil {
 		_conf.Warn.log(WARN, "", msg)
-	}
-	if _conf2 != nil {
+	} else if _conf != nil {
+		_conf.Warn.log(WARN, "", msg)
+	} else if _conf2 != nil {
 		_conf2.Warn.log(WARN, "", msg)
 	}
 }
