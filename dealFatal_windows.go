013695b98f54aba09dcce473570f6349e1948a01
bxl <>
2023-11-22 16:40:45 +0800

    Update dealFatal_windows.go

--- dealFatal_windows.go
+++ dealFatal_windows.go
@@ -1,23 +1,17 @@
 package log
 
-import (
-	"fmt"
-	"os"
-	"syscall"
-)
-
 func dealFatal() {
-	if !Exist(path) {
-		err := os.Mkdir(path, 0666)
-		if err != nil {
-			fmt.Println("deal fatal failed! ", err)
-			os.Exit(1)
-		}
-	}
-	logFile, err := os.OpenFile(path+"/fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Println("deal fatal failed! ", err)
-		os.Exit(1)
-	}
-	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
+	//if !Exist(path) {
+	//	err := os.Mkdir(path, 0666)
+	//	if err != nil {
+	//		fmt.Println("deal fatal failed! ", err)
+	//		os.Exit(1)
+	//	}
+	//}
+	//logFile, err := os.OpenFile(path+"/fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	//if err != nil {
+	//	fmt.Println("deal fatal failed! ", err)
+	//	os.Exit(1)
+	//}
+	//syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
 }
