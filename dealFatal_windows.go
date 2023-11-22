091d75ae95c9c2d72561ac955108de2bc6dc0a0b
bxl <>
2023-11-22 16:28:44 +0800

    udpate

---
+++ dealFatal_windows.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func dealFatal() {
+	if !Exist(path) {
+		err := os.Mkdir(path, 0666)
+		if err != nil {
+			fmt.Println("deal fatal failed! ", err)
+			os.Exit(1)
+		}
+	}
+	logFile, err := os.OpenFile(path+"/fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("deal fatal failed! ", err)
+		os.Exit(1)
+	}
+	syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
+}
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -411,20 +410,4 @@ func createLevelLogger(level Level, t time.Time) *log.Logger {
 		return fileLogger
 	}
 	return &log.Logger{}
-}
-
-func dealFatal() {
-	if !Exist(path) {
-		err := os.Mkdir(path, 0666)
-		if err != nil {
-			fmt.Println("deal fatal failed! ", err)
-			os.Exit(1)
-		}
-	}
-	logFile, err := os.OpenFile(path+"/fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Println("deal fatal failed! ", err)
-		os.Exit(1)
-	}
-	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
 }
